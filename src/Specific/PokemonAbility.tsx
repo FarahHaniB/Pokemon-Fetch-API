3b49467a2ec9328923ba546722c19d96a1f7640c
FarahHaniB <109828677+FarahHaniB@users.noreply.github.com>
2023-10-20 14:21:08 +0800

    Edited styling

--- src/Specific/PokemonAbility.tsx
+++ src/Specific/PokemonAbility.tsx
@@ -3,7 +3,7 @@ import { Box, Text } from "@chakra-ui/react";
 
 function PokemonAbility({ ability }: { ability: string[] }) {
   return (
-    <Box p="8px" m="5px" minWidth='120px' bg="#bbe4e9" borderRadius="md">
+    <Box p="8px" m="5px" minWidth='120px' bg="#cadefc" borderRadius="md">
       <Text align="center">{ability}</Text>
     </Box>
   );
